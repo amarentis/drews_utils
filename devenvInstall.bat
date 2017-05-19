618c0c3df1819369c2791f4965ff0aadd079bc6f
amarentis <amarentis@users.noreply.github.com>
2017-05-18 22:22:39 -0400

    Added vagrant plugin vagrant-omnibus

--- devenvInstall.bat
+++ devenvInstall.bat
@@ -6,8 +6,6 @@
 @REM 
 @REM Sets up basic Chef Dev environment and a few handy tools 
 
-
-
 @REM Install Chocolatey
 CALL :banner "Installing Chocolatey Windows Package Manager"
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
@@ -18,6 +16,10 @@ CALL :banner "Installing Dev Tools....."
 choco install git virtualbox virtualbox.extensionpack vagrant docker filezilla chefdk putty gradle wget notepadplusplus fiddler4 choco install sublimetext3 -y
 echo "Done!"
 
+@REM Install Vagrant Plugins
+CALL :banner "Installing additional required vagrant plugins needed on windows"
+vagrant plugin install vagrant-omnibus
+echo "Done!"
 
 :: Functions
 :banner
